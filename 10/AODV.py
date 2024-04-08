9788cd8ae14f566a1bc78dabc6ef605560646f75
i-am-stark <amanmaury36@gmail.com>
2024-04-08 22:41:03 +0530

    AODV DSDV

---
+++ 10/AODV.py
@@ -0,0 +1,58 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import heapq
+
+class AODV:
+    def __init__(self):
+        self.routing_table = {}
+        self.seq_num = 0  # Initial sequence number
+
+    def update_routing_table(self, node, destination, next_hop, num_hops):
+        if node not in self.routing_table:
+            self.routing_table[node] = {}
+        
+        if destination in self.routing_table[node]:
+            current_next_hop, current_num_hops, current_seq_num = self.routing_table[node][destination]
+            if num_hops < current_num_hops or (num_hops == current_num_hops and self.seq_num > current_seq_num):
+                self.routing_table[node][destination] = (next_hop, num_hops, self.seq_num)
+        else:
+            self.routing_table[node][destination] = (next_hop, num_hops, self.seq_num)
+
+
+    def visualize_routing_table(self):
+        print("Routing Tables:")
+        for node in self.routing_table:
+            print(f"Node: {node}")
+            print("Destination\tNext Hop\tNum Hops\tSeq Num")
+            for destination, (next_hop, num_hops, seq_num) in self.routing_table[node].items():
+                print(f"{destination}\t\t{next_hop}\t\t{num_hops}\t\t{seq_num}")
+            print()
+
+    def visualize_network_graph(self):
+        G = nx.DiGraph()
+        for node in self.routing_table:
+            for destination, (next_hop, num_hops, seq_num) in self.routing_table[node].items():
+                G.add_edge(node, next_hop, label=f"{destination}, {num_hops + 1}")
+
+        plt.figure(figsize=(10, 8))
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=12, font_weight='bold')
+        edge_labels = nx.get_edge_attributes(G, 'label')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.title("Network Graph with Routing Information")
+        plt.show()
+
+# Example usage
+aodv = AODV()
+aodv.update_routing_table('A', 'B', 'B', 0)
+aodv.update_routing_table('A', 'C', 'C', 0)
+aodv.update_routing_table('B', 'D', 'D', 1)
+aodv.update_routing_table('C', 'D', 'B', 2)
+
+# Display routing table of each node
+aodv.visualize_routing_table()
+
+# Visualize the network graph
+aodv.visualize_network_graph()
+
+
